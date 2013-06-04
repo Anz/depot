19475c0ef09640edb6f44bf0c6b449ced3e438dd
Anz <knecht_roger@hotmail.com>
2013-06-04 17:41:56 +0200

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,9 @@ Product.create(title: 'The C++ Programming Language',
   price: 55.26)
 # . . .
 
-Product.create(title: 'The Java Programming Language',
-  description: 
-    %{<p>
-
-Direct from the creators of the Java™ programming language, the completely revised fourth edition of The Java™ Programming Language is an indispensable resource for novice and advanced programmers alike.
-
-Developers around the world have used previous editions to quickly gain a deep understanding of the Java programming language, its design goals, and how to use it most effectively in real-world development. Now, Ken Arnold, James Gosling, and David Holmes have updated this classic to reflect the major enhancements in Java™ 2 Standard Edition 5.0 (J2SE™ 5.0).
-
-The authors systematically cover most classes in Java’s main packages, java.lang.*, java.util, and java.io, presenting in-depth explanations of why these classes work as they do, with informative examples. Several new chapters and major sections have been added, and every chapter has been updated to reflect today’s best practices for building robust, efficient, and maintainable Java software.
-
-Key changes in this edition include
-
-    New chapters on generics, enums, and annotations, the most powerful new language features introduced in J2SE 5.0
-    Changes to classes and methods throughout to reflect the addition of generics
-    Major new sections on assertions and regular expressions
-    Coverage of all the new language features, from autoboxing and variable argument methods to the enhanced for-loop and covariant return types
-    Coverage of key new classes, such as Formatter and Scanner
-
-The Java™ Programming Language, Fourth Edition, is the definitive tutorial introduction to the Java language and essential libraries and an indispensable reference for all programmers, including those with extensive experience. It brings together insights you can only get from the creators of Java: insights that will help you write software of exceptional quality.
-</p>},
-  image_url: 'http://ecx.images-amazon.com/images/I/51FYWA71SWL._SX240_.jpg',
-  price: 51.96)
-  
   
   Product.create(title: 'The Ruby Programming Language ',
   description:
-    %{<p>David Flanagan is a computer programmer who spends most of his time writing about JavaScript and Java. His books with O'Reilly include JavaScript: The Definitive Guide, JavaScript Pocket Reference, Java in a Nutshell, Java Examples in a Nutshell, and Java Foundation Classes in a Nutshell. David has a degree in computer science and engineering from the Massachusetts Institute of Technology. He lives with his wife and children in the U.S. Pacific Northwest bewteen the cities of Seattle, Washington and Vancouver, British Columbia. David has a blog at www.davidflanagan.com.
-
-Yukihiro Matsumoto ("Matz"), the creator of Ruby, is a professional programmer who worked for the Japanese open source company, netlab.jp. Matz is also known as one of the open source evangelists in Japan. He's released several open source products, including cmail, the emacs-based mail user agent, written entirely in emacs lisp. Ruby is his first piece of software that has become known outside of Japan.</p>},
+    %{<p>David Flanagan is a computer programmer who spends most of his time writing about JavaScript and Java. His books with O'Reilly include JavaScript: The Definitive Guide, JavaScript Pocket Reference, Java in a Nutshell, Java Examples in a Nutshell, and Java Foundation Classes in a Nutshell. David has a degree in computer science and engineering from the Massachusetts Institute of Technology. He lives with his wife and children in the U.S. Pacific Northwest bewteen the cities of Seattle, Washington and Vancouver, British Columbia. David has a blog at www.davidflanagan.com. Yukihiro Matsumoto ("Matz"), the creator of Ruby, is a professional programmer who worked for the Japanese open source company, netlab.jp. Matz is also known as one of the open source evangelists in Japan. He's released several open source products, including cmail, the emacs-based mail user agent, written entirely in emacs lisp. Ruby is his first piece of software that has become known outside of Japan.</p>},
   image_url: 'http://ecx.images-amazon.com/images/I/51wlknppToL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_SX240_SY320_CR,0,0,240,320_SH20_OU01_.jpg',
   price: 25)
