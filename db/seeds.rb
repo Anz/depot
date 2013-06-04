71c2d9203862ab13759ac329793f14709e0dd517
Anz <knecht_roger@hotmail.com>
2013-06-04 17:20:23 +0200

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,45 @@
 Product.delete_all
-Product.create(title: 'CoffeeScript',
+Product.create(title: 'C Programming Language',
   description: 
-    %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
-  image_url:   'cs.jpg',    
-  price: 36.00)
+    %{<p>The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C's rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs. For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on.</p>},
+  image_url:   'http://ecx.images-amazon.com/images/I/41AuBvXKY7L._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_SX240_SY320_CR,0,0,240,320_SH20_OU01_.jpg',    
+  price: 49.51)
 # . . .
-Product.create(title: 'Programming Ruby 1.9',
+Product.create(title: 'The C++ Programming Language',
   description:
-    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
-  image_url: 'ruby.jpg',
-  price: 49.95)
+    %{<p>Written by the inventor of the language, the book is the defining, classic text on the language that has become central to software development over the past five years. This third edition incorporates additions and changes on a major scale. In particular, the new edition is based on the ANSI/ISO C++ final draft with its many new language features - templates, exceptions, namespaces, and run-time type identification, to name a few - in addition to the C++ Standard Template Library that has revolutionized C++ development. * Throughout, the book does far more than merely describe every element of the language. The focus is on showing how the language is used as a tool for design and programming, and teaching the basic concepts programmers need to master C++. With this third edition, Stroustrup has made the book even more accessible to those new to the language while adding information and techniques that even expert C++ programmers will find invaluable.</p>},
+  image_url: 'http://ecx.images-amazon.com/images/I/519iA57UxgL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_SX240_SY320_CR,0,0,240,320_SH20_OU01_.jpg',
+  price: 55.26)
 # . . .
 
-Product.create(title: 'Rails Test Prescriptions',
+Product.create(title: 'The Java Programming Language',
   description: 
     %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
-  image_url: 'rtp.jpg',
-  price: 34.95)
+
+Direct from the creators of the Java™ programming language, the completely revised fourth edition of The Java™ Programming Language is an indispensable resource for novice and advanced programmers alike.
+
+Developers around the world have used previous editions to quickly gain a deep understanding of the Java programming language, its design goals, and how to use it most effectively in real-world development. Now, Ken Arnold, James Gosling, and David Holmes have updated this classic to reflect the major enhancements in Java™ 2 Standard Edition 5.0 (J2SE™ 5.0).
+
+The authors systematically cover most classes in Java’s main packages, java.lang.*, java.util, and java.io, presenting in-depth explanations of why these classes work as they do, with informative examples. Several new chapters and major sections have been added, and every chapter has been updated to reflect today’s best practices for building robust, efficient, and maintainable Java software.
+
+Key changes in this edition include
+
+    New chapters on generics, enums, and annotations, the most powerful new language features introduced in J2SE 5.0
+    Changes to classes and methods throughout to reflect the addition of generics
+    Major new sections on assertions and regular expressions
+    Coverage of all the new language features, from autoboxing and variable argument methods to the enhanced for-loop and covariant return types
+    Coverage of key new classes, such as Formatter and Scanner
+
+The Java™ Programming Language, Fourth Edition, is the definitive tutorial introduction to the Java language and essential libraries and an indispensable reference for all programmers, including those with extensive experience. It brings together insights you can only get from the creators of Java: insights that will help you write software of exceptional quality.
+</p>},
+  image_url: 'http://ecx.images-amazon.com/images/I/51FYWA71SWL._SX240_.jpg',
+  price: 51.96)
+  
+  
+  Product.create(title: 'The Ruby Programming Language ',
+  description:
+    %{<p>David Flanagan is a computer programmer who spends most of his time writing about JavaScript and Java. His books with O'Reilly include JavaScript: The Definitive Guide, JavaScript Pocket Reference, Java in a Nutshell, Java Examples in a Nutshell, and Java Foundation Classes in a Nutshell. David has a degree in computer science and engineering from the Massachusetts Institute of Technology. He lives with his wife and children in the U.S. Pacific Northwest bewteen the cities of Seattle, Washington and Vancouver, British Columbia. David has a blog at www.davidflanagan.com.
+
+Yukihiro Matsumoto ("Matz"), the creator of Ruby, is a professional programmer who worked for the Japanese open source company, netlab.jp. Matz is also known as one of the open source evangelists in Japan. He's released several open source products, including cmail, the emacs-based mail user agent, written entirely in emacs lisp. Ruby is his first piece of software that has become known outside of Japan.</p>},
+  image_url: 'http://ecx.images-amazon.com/images/I/51wlknppToL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_SX240_SY320_CR,0,0,240,320_SH20_OU01_.jpg',
+  price: 25)
